640073c893f4cfbedd39863d1a5c16a0d5520d22
Shyam Patil <shyampatil9284@gmail.com>
2025-03-12 07:41:42 +0000

    demo project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({ 
   selector: 'app-component',
   template: `
-    <h1>Hello World</h1>
+    <h1>Welcome...!</h1>
+    <h2>This is Shyam Patil's first live Project</h2>
   `
 })
 export class AppComponent {
