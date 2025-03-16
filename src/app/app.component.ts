63d3e46956df29dba06c85cf756106778f0c109b
shyam <shyampatil9283@gmail.com>
2025-03-16 12:25:12 +0000

    new change by shyam

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <h1>Welcome...!</h1>
     <h2>This is Shyam Patil's first live Project</h2>
+    <h2>New change by Shyam Patil</h2>
   `
 })
 export class AppComponent {
